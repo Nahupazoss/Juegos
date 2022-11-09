18300eaf0fa6b22ac8d62728176be6d8ea7e9eb8
Nahuel Pazos <rodrigopazos9@gmail.com>
2022-11-09 00:27:39 -0300

    2

---
+++ Juego_Auto/JuegoAuto/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace JuegoAuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Auto = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Auto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Auto
+            // 
+            this.Auto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Auto.Image = ((System.Drawing.Image)(resources.GetObject("Auto.Image")));
+            this.Auto.Location = new System.Drawing.Point(180, 661);
+            this.Auto.Name = "Auto";
+            this.Auto.Size = new System.Drawing.Size(149, 166);
+            this.Auto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Auto.TabIndex = 0;
+            this.Auto.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(367, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 2;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(180, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 862);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 853);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(440, 854);
+            this.Controls.Add(this.Auto);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DRK Autos Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.Auto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Auto;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Juego_Auto/JuegoAuto/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoAuto
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> listObstaculos;
+        Random randomObstaculos = new Random();
+        int velocidad = 3;
+        int animacionAuto = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listObstaculos = new List<PictureBox>();
+            listObstaculos.Add(new PictureBox());
+            listObstaculos.Add(new PictureBox());
+            listObstaculos.Add(new PictureBox());
+            CrearObstaculo(listObstaculos, this, 10, 180);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        public void CrearObstaculo(List<PictureBox> listObstaculos, Form panelJuegoUno, int distanciauno, int distanciados)
+        {
+            int numeroAuto = 1;
+            int tipoObstaculo = randomObstaculos.Next(1, 3);
+            int ubicacionObstaculo = randomObstaculos.Next(1, 3);
+
+            int distanciaUbicacionObstaculo = (ubicacionObstaculo == 1) ? distanciauno : distanciados;
+
+            PictureBox pb = new PictureBox();
+            pb.Location = new Point(distanciaUbicacionObstaculo, 0);
+            pb.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("Obstaculo"+numeroAuto+tipoObstaculo);
+            pb.BackColor = Color.Transparent;
+            pb.Tag = numeroAuto + "_" + tipoObstaculo;
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            listObstaculos.Add(pb);
+            panelJuegoUno.Controls.Add(pb);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Auto.Image = (animacionAuto == 0) ? Properties.Resources.Auto : Properties.Resources.Auto;
+            animacionAuto = (animacionAuto == 0) ? 1 : 0;
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int cambioCarril = (Auto.Location.X == 180) ? 10 : 180;
+            Auto.Location = new Point(cambioCarril, Auto.Location.Y);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            /*foreach (PictureBox imagenAuto in listObstaculos)
+            {
+                int movimientoY;
+                movimientoY = imagenAuto.Location.Y;
+                movimientoY = movimientoY + velocidad;
+                imagenAuto.Location = new Point(imagenAuto.Location.X, movimientoY);
+            }
+
+            if(listObstaculos.Count > 0)
+            {
+                if (listObstaculos[(listObstaculos.Count) - 1].Location.Y > 259)
+                {
+                    CrearObstaculo(listObstaculos, this, 10, 80);
+                }
+            }
+
+            
+            if(listObstaculos.Count > 0)
+            {
+                for (int i = 0; i < listObstaculos.Count; i++)
+                {
+                    //si los elementos no chocaron
+                    if (listObstaculos[i].Tag.ToString() == "1_1")
+                    {
+                        ReiniciarJuego();
+                    }
+                    this.Controls.Remove(listObstaculos[i]);
+                    listObstaculos.Remove(listObstaculos[i]);
+
+                    //si los elementos chocaron
+                    if (listObstaculos[i].Tag.ToString() == "1_1")
+                    {
+                        this.Controls.Remove(listObstaculos[i]);
+                        int totalPuntos = Convert.ToInt32(label1.Text) + 1;
+                        if(totalPuntos % 2 == 0)
+                        {
+                            velocidad++;//incrementa velocidad
+                        }
+                        label1.Text = totalPuntos.ToString();
+                        listObstaculos.Remove(listObstaculos[i]);
+                    }
+                    else
+                    {
+                        this.Controls.Remove(listObstaculos[i]);
+                        listObstaculos.Remove(listObstaculos[i]);
+                        ReiniciarJuego();
+                    }
+                }
+            }
+
+          
+         */  
+        }
+
+        public void ReiniciarJuego()
+        {
+            label1.Text = "0";
+            velocidad = 3;
+        }
+    }
+}
+
+
+
+
